07e6c5d3258c721fe1c47783625c0adb301fe4b7
Joshua <joshchwang@gmail.com>
2017-10-03 18:18:17 -0700

    Kookoo grill files added

--- app/app.js
+++ app/app.js
@@ -1,19 +1,11 @@
 var express = require('express');
-var app     = express();
-var path    = require('path');
+var app = express();
+var path = require('path');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/views/index.html'));
-});
-
-app.get('/menu', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/views/menu.html'));
-});
-
-app.get('/our-team', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/views/team.html'));
 });
 
 var port = process.env.PORT || 1337;
--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,6 +1,26 @@
+// $(document).ready(function(){
+//    var scroll_start = 0;
+//    var startchange = $('#about-container');
+//    var offset = startchange.offset();
+//
+//    $(document).scroll(function() {
+//       scroll_start = $(this).scrollTop();
+//
+//       if(scroll_start > offset.top) {
+//           $('.navbar').css('background-color', '#d72a45');
+//           $('.nav-links').css('color', '#FDF9FD');
+//           $('.my-name').css('color', '#FDF9FD');
+//           $('.social-links').css('color', '#FDF9FD');
+//           $('.navbar').css('visibility', 'visible');
+//        } else {
+//           $('.navbar').css('visibility', 'hidden');
+//        }
+//    });
+// });
+
 // GOOGLE MAP INTEGRATION
 function initMap() {
-  var mapDiv  = document.getElementById('map');
+  var mapDiv = document.getElementById('map');
   var latLong = { lat: 33.835755, lng: -117.916293 }
 
   var map = new google.maps.Map(mapDiv, {
@@ -17,38 +37,62 @@ function initMap() {
 }
 
 // NAV LINKS
-var aboutLink = document.getElementById('about');
-var menuLink  = document.getElementById('menu');
-var findLink  = document.getElementById('findUs');
+var aboutLink = document.getElementById('aboutLink');
+var menuLink = document.getElementById('menuLink');
+var contactLink = document.getElementById('contactLink');
 
-var about = document.getElementById('aboutContainer');
-var menu  = document.getElementById('menuContainer');
-var find  = document.getElementById('mapContainer');
+var fixedAboutLink = document.getElementById('fixedAboutLink');
+var fixedMenuLink = document.getElementById('fixedMenuLink');
+var fixedContactLink = document.getElementById('fixedContactLink');
+
+var about = document.getElementById('about-container');
+var menu = document.getElementById('menu-container');
+var find = document.getElementById('map-container');
+
+// scroll.animateScroll(about, aboutLink, options);
+
+function scrollAbout() {
+  var scroll = new SmoothScroll();
+  var options = { speed: 1000, easing: 'easeOutCubic' }
+
+  var aboutLink = document.querySelector('aboutLink');
+  var about = document.getElementById('about-container');
+
+  scroll.animateScroll(about, aboutLink, options);
+}
+
+function scrollMenu() {
+  var scroll = new SmoothScroll();
+  var options = { speed: 1500, easing: 'easeOutCubic' }
+
+  var aboutLink = document.querySelector('menuLink');
+  var about = document.getElementById('menu-container');
+
+  scroll.animateScroll(about, aboutLink, options);
+}
+
+function scrollContact() {
+  var scroll = new SmoothScroll();
+  var options = { speed: 2000, easing: 'easeOutCubic' }
+
+  var aboutLink = document.querySelector('contactLink');
+  var about = document.getElementById('contact-container');
+
+  scroll.animateScroll(about, aboutLink, options);
+}
 
 aboutLink.addEventListener('click', scrollAbout);
 menuLink.addEventListener('click', scrollMenu);
-findLink.addEventListener('click', scrollFind);
+contactLink.addEventListener('click', scrollContact);
 
-function scrollAbout(e) {
-  e.preventDefault;
+fixedAboutLink.addEventListener('click', scrollAbout);
+fixedMenuLink.addEventListener('click', scrollMenu);
+fixedContactLink.addEventListener('click', scrollContact);
 
-  $('html, body').animate({
-      scrollTop: $(about).offset().top
-  }, 750);
-}
+// SCROLL REVEAL
+window.sr = ScrollReveal();
 
-function scrollMenu(e) {
-  e.preventDefault;
-
-  $('html, body').animate({
-      scrollTop: $(menu).offset().top
-  }, 750);
-}
-
-function scrollFind(e) {
-  e.preventDefault;
-
-  $('html, body').animate({
-      scrollTop: $(find).offset().top
-  }, 750);
-}
+sr.reveal('#about-container', { duration: 300 });
+sr.reveal('#menu-container', { duration: 300 });
+sr.reveal('#gallery-row', { duration: 300 });
+sr.reveal('#contact-row', { duration: 300 });
